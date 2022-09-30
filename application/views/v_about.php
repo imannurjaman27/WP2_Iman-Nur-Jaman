65a4389aff3aefb39fb80d7096548b4068ce4ebd
imannurjaman27 <imannurjaman27@gmail.com>
2022-09-30 15:20:04 +0700

    tugas pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1> 
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Iman</li>
+        <li>Nama Belakang : Nur Jaman</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Raya Desa Kapur Kec. Sungai Raya, Kab. Kubu raya</li>
+    </ul>
+
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Pontianak</li>
+    </ul>
+    
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>Bermain Game Online</li>
+    </ul>
